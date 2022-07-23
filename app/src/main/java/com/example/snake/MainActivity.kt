a70b56597b177221075ffc4e41856dc01f9dd322
syoma <skat.apps@gmail.com>
2022-07-23 14:16:43 +0300

    add buttons arrows

--- app/src/main/java/com/example/snake/MainActivity.kt
+++ app/src/main/java/com/example/snake/MainActivity.kt
@@ -3,27 +3,29 @@ package com.example.snake
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
 import com.example.snake.data.GameEngine
 import com.example.snake.ui.theme.SnakeTheme
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.example.snake.data.SnakeDirection
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +33,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             SnakeTheme {
                 val viewModel: GameEngine = viewModel()
-                Board(viewModel = viewModel)
+
+                Column {
+                    Board(viewModel = viewModel)
+                    Arrows(viewModel = viewModel)
+                }
                 viewModel.start()
             }
         }
@@ -49,30 +55,72 @@ private fun Board(viewModel: GameEngine) {
         Box(
             Modifier
                 .size(maxWidth)
-                .border(BorderStroke(2.dp, Color.Gray), RectangleShape)) {
-            Box(
-                Modifier
-                    .offset(
-                        x = itemSize * state.value!!.food.first,
-                        y = itemSize * state.value!!.food.second
-                    )
-                    .size(itemSize)
-                    .background(Color.Magenta, CircleShape)
-            )
+                .border(BorderStroke(2.dp, Color.Gray), RectangleShape)
+        ) {
 
-            state.value!!.snake.forEach { itemSnake ->
-                Box(
-                    Modifier
-                        .offset(
-                            x = itemSize * itemSnake.first,
-                            y = itemSize * itemSnake.second
-                        )
-                        .size(itemSize)
-                        .background(Color.Green, RoundedCornerShape(4.dp))
-                )
-            }
+            Food(itemSize = itemSize, food = state.value!!.food)
+            Snake(snake = state.value!!.snake, itemSize = itemSize)
         }
     }
 
 }
 
+@Composable
+private fun Food(itemSize: Dp, food: Pair<Int, Int>) {
+
+    Box(
+        Modifier
+            .offset(
+                x = itemSize * food.first,
+                y = itemSize * food.second
+            )
+            .size(itemSize)
+            .background(Color.Magenta, CircleShape)
+    )
+}
+
+@Composable
+private fun Snake(snake: List<Pair<Int, Int>>, itemSize: Dp) {
+    snake.forEach { itemSnake ->
+        Box(
+            Modifier
+                .offset(
+                    x = itemSize * itemSnake.first,
+                    y = itemSize * itemSnake.second
+                )
+                .size(itemSize)
+                .background(Color.Green, RoundedCornerShape(4.dp))
+        )
+    }
+}
+
+@Composable
+private fun Arrows(viewModel: GameEngine) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Arrow(R.drawable.ic_up_arrow) { viewModel.updateDirection(SnakeDirection.TOP) }
+        Row {
+            Arrow(R.drawable.ic_baseline_keyboard_arrow_left_24) {
+                viewModel.updateDirection(
+                    SnakeDirection.LEFT
+                )
+            }
+
+            Arrow(painterId = R.drawable.ic_launcher_background) {}
+
+            Arrow(R.drawable.ic_baseline_keyboard_arrow_right_24) {
+                viewModel.updateDirection(
+                    SnakeDirection.RIGHT
+                )
+            }
+        }
+        Arrow(R.drawable.ic_down_arrow) { viewModel.updateDirection(SnakeDirection.BOTTOM) }
+
+    }
+}
+
+@Composable
+private fun Arrow(painterId: Int, onClick: () -> Unit) {
+    Button(onClick = { onClick() }, Modifier.padding(8.dp)) {
+        Icon(painter = painterResource(id = painterId), contentDescription = "btn", modifier = Modifier.size(30.dp))
+    }
+}
