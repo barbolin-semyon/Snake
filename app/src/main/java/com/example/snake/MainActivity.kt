88ca79a30f7d3651e4c10f33059629f11242c2c8
syoma <skat.apps@gmail.com>
2022-07-23 13:44:22 +0300

    draw snake, food

--- app/src/main/java/com/example/snake/MainActivity.kt
+++ app/src/main/java/com/example/snake/MainActivity.kt
@@ -3,41 +3,76 @@ package com.example.snake
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import com.example.snake.data.GameEngine
 import com.example.snake.ui.theme.SnakeTheme
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SnakeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val viewModel: GameEngine = viewModel()
+                Board(viewModel = viewModel)
+                viewModel.start()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun Board(viewModel: GameEngine) {
+    val state = viewModel.stateGame.observeAsState()
+
+    BoxWithConstraints(Modifier.padding(16.dp)) {
+        val itemSize = maxWidth / viewModel.BOARD_SIZE
+
+        Box(
+            Modifier
+                .size(maxWidth)
+                .border(BorderStroke(2.dp, Color.Gray), RectangleShape)) {
+            Box(
+                Modifier
+                    .offset(
+                        x = itemSize * state.value!!.food.first,
+                        y = itemSize * state.value!!.food.second
+                    )
+                    .size(itemSize)
+                    .background(Color.Magenta, CircleShape)
+            )
+
+            state.value!!.snake.forEach { itemSnake ->
+                Box(
+                    Modifier
+                        .offset(
+                            x = itemSize * itemSnake.first,
+                            y = itemSize * itemSnake.second
+                        )
+                        .size(itemSize)
+                        .background(Color.Green, RoundedCornerShape(4.dp))
+                )
+            }
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SnakeTheme {
-        Greeting("Android")
-    }
-}
