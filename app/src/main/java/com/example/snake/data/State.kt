a407c4654510554b38bcec0c8958edeb3b76eff3
syoma <skat.apps@gmail.com>
2022-07-23 14:15:39 +0300

    change val to var direction

--- app/src/main/java/com/example/snake/data/State.kt
+++ app/src/main/java/com/example/snake/data/State.kt
@@ -3,5 +3,5 @@ package com.example.snake.data
 data class State(
     val food: Pair<Int, Int>,
     val snake: List<Pair<Int, Int>>,
-    val direction: SnakeDirection
+    var direction: SnakeDirection
 )
